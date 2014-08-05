891dd18fa5afecea72456dea8d19eb92dd809fc9
Dizz Err <dizzerr@gmail.com>
2014-08-05 14:27:42 +0300

    [DF-185] wzzip usage replaced with 7z

--- Tools/ResourceEditor/Teamcity/packAppQt.bat
+++ Tools/ResourceEditor/Teamcity/packAppQt.bat
@@ -21,4 +21,4 @@ xcopy /e ..\..\..\..\dava.resourceeditor.beast\*.* app\ResourceEditor\dava.resou
 xcopy ..\..\..\..\dava.framework\Libs\fmod\bin\fmodex.dll app\ResourceEditor\dava.framework\Tools\ResourceEditor
 xcopy ..\..\..\..\dava.framework\Libs\fmod\bin\fmod_event.dll app\ResourceEditor\dava.framework\Tools\ResourceEditor
 
-wzzip -p -r %1 app\*.*	
+7z a -tzip %1 app\*.*	
