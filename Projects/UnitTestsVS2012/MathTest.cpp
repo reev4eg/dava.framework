c7c2bd8fac66dcf848ed05dc7a21a8018467f857
leanid <leanid.chaika@gmail.com>
2014-10-15 10:42:48 +0300

    DF-5130 start with Catch - unit test lib, write minimal test to check works

--- Projects/UnitTestsVS2012/MathTest.cpp
+++ Projects/UnitTestsVS2012/MathTest.cpp
@@ -1,30 +1,27 @@
 #include "stdafx.h"
-#include "CppUnitTest.h"
-
 #include "DAVAEngine.h"
-using namespace DAVA;
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+#include "catch.hpp"
 
 namespace
 {
-	float32 SquareDist(const Matrix4& m1, const Matrix4& m2)
+	DAVA::float32 SquareDist(const DAVA::Matrix4& m1, const DAVA::Matrix4& m2)
 	{
-		float32 result = 0.0f;
-		for (uint32 i = 0; i < 4; ++i)
-			for (uint32 j = 0; j < 4; ++j)
-				result += Abs(m1(i, j) - m2(i, j)) * Abs(m1(i, j) - m2(i, j));
+		DAVA::float32 result = 0.0f;
+		for (DAVA::uint32 i = 0; i < 4; ++i)
+			for (DAVA::uint32 j = 0; j < 4; ++j)
+				result += DAVA::Abs(m1(i, j) - m2(i, j)) * DAVA::Abs(m1(i, j) - m2(i, j));
 
 		return result;
 	}
 
-	float32 TestMatrixDecomposition(const Matrix4& mat)
+	DAVA::float32 TestMatrixDecomposition(const DAVA::Matrix4& mat)
 	{
-		Vector3 position, scale;
-		Quaternion rotation;
+		DAVA::Vector3 position, scale;
+		DAVA::Quaternion rotation;
 		mat.Decomposition(position, scale, rotation);
 
-		Matrix4 reconstructedMatrix = rotation.GetMatrix() * Matrix4::MakeScale(scale);
+		DAVA::Matrix4 reconstructedMatrix = rotation.GetMatrix() * DAVA::Matrix4::MakeScale(scale);
 		reconstructedMatrix.SetTranslationVector(position);
 
 		return SquareDist(reconstructedMatrix, mat);
@@ -32,26 +29,20 @@ namespace
 }
 
 
-namespace UnitTestsVS2012
+namespace MathTest
 {
-	TEST_CLASS(MathTest)
+	TEST_CASE("MathTest", "[math]")
 	{
-	public:
-		
-		TEST_METHOD(MatrixTestFunction)
-		{
-			Assert::IsTrue(TestMatrixDecomposition(Matrix4::MakeTranslation(Vector3(10.0f, 0.0f, 0.0f))) < 0.0001f);
-			Assert::IsTrue(TestMatrixDecomposition(Matrix4::MakeRotation(Vector3(1.0f, 0.0f, 0.0f), PI_05)) < 0.0001f);
-			Assert::IsTrue(TestMatrixDecomposition(Matrix4::MakeScale(Vector3(3.0f, 3.0f, 3.0f))) < 0.0001f);
+		REQUIRE(TestMatrixDecomposition(DAVA::Matrix4::MakeTranslation(DAVA::Vector3(10.0f, 0.0f, 0.0f))) < 0.0001f);
+		REQUIRE(TestMatrixDecomposition(DAVA::Matrix4::MakeRotation(DAVA::Vector3(1.0f, 0.0f, 0.0f), DAVA::PI_05)) < 0.0001f);
+		REQUIRE(TestMatrixDecomposition(DAVA::Matrix4::MakeScale(DAVA::Vector3(3.0f, 3.0f, 3.0f))) < 0.0001f);
 
-			Vector3 axis(0.0f, 1.0f, 1.0f);
-			axis.Normalize();
-			Assert::IsTrue(
-				TestMatrixDecomposition(
-				Matrix4::MakeTranslation(Vector3(10.0f, 0.0f, 0.0f)) *
-				Matrix4::MakeRotation(axis, PI_05 * 0.25f) *
-				Matrix4::MakeScale(Vector3(3.0f, 3.0f, 3.0f))) < 0.0001f);
-		}
-
-	};
+		DAVA::Vector3 axis(0.0f, 1.0f, 1.0f);
+		axis.Normalize();
+		REQUIRE(
+			TestMatrixDecomposition(
+			DAVA::Matrix4::MakeTranslation(DAVA::Vector3(10.0f, 0.0f, 0.0f)) *
+			DAVA::Matrix4::MakeRotation(axis, DAVA::PI_05 * 0.25f) *
+			DAVA::Matrix4::MakeScale(DAVA::Vector3(3.0f, 3.0f, 3.0f))) < 0.0001f);
+	}
 }
