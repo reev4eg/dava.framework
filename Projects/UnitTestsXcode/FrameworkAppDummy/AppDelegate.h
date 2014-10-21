1709f0e250882608f4056731ec2f59834b4521ce
leanid <leanid.chaika@gmail.com>
2014-10-21 16:53:13 +0300

    DP-5130 add several types of sub targets for different tests of XCTestUnit.

---
+++ Projects/UnitTestsXcode/FrameworkAppDummy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FrameworkAppDummy
+//
+//  Created by user123 on 10/21/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Projects/UnitTestsXcode/FrameworkAppDummy/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  FrameworkAppDummy
+//
+//  Created by user123 on 10/21/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
